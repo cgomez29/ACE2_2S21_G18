fb7a9bf2091226e7263cd1559898eb711dbd79a7
Elmer Gustavo Sanchez Garcia <54461105+gstavosanchez@users.noreply.github.com>
2021-08-14 23:33:54 -0600

    Agrego código temperatura-humedad
    Instalar la librería DHTlib, para leer los valores del sensor, se uso el pin 2 del arduino.

---
+++ Practica 1/temperaturaHumedad.ino
@@ -0,0 +1,32 @@
+#include <dht.h>
+dht DHT;
+#define DHT11_PIN 2
+
+void setup()
+{
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    Serial.println(getTemperatureHumidity());
+    delay(2000);
+}
+
+String getTemperatureHumidity()
+{
+    String temperature = "\"temperature\": ";
+    String humidity = "\"humedad\": ";
+
+    // Lee la informacion que envia el sensor
+    DHT.read11(DHT11_PIN);
+
+    // TEMPERATURA
+    temperature.concat(DHT.temperature);
+
+    // HUMEDAD RELATIVA
+    humidity.concat(DHT.humidity);
+
+    // JSON
+    return ("{" + temperature + ", " + humidity + " }");
+}
