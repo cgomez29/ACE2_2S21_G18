f8b6a9eb6f977f32f75ae9e50cffcd9e38acafa1
PabloC54 <3091865790609@ingenieria.usac.edu.gt>
2021-08-20 13:55:05 -0600

    Serial port integration

---
+++ Practica 1/serial/main.js
@@ -0,0 +1,31 @@
+import serialPort from 'serialport'
+import readLine from '@serialport/parser-readline'
+import axios from 'axios'
+
+import { PORT } from './api/constants.js'
+const URL = `https://localhost:${PORT}`
+
+const serial = new serialPort('COM1', { baudRate: 9600 })
+const parser = serial.pipe(new readLine({ delimiter: '\n' }))
+
+serial.on('open', () => {
+  console.log('opened')
+
+  axios
+    .get(URL)
+    .then((res) => {
+      console.log(res)
+    })
+    .catch((error) => console.log(error))
+})
+
+serial.on('data', (data) => {
+  console.log(`se recibió ${data}`)
+
+  axios
+    .post(URL, { data })
+    .then((res) => {
+      console.log(res)
+    })
+    .catch((error) => console.log(error))
+})
