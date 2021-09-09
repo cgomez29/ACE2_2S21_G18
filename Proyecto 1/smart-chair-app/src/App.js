84a6550df6b76a074378e501768fcddca7e8f57c
Cristian Gómez <2912196670108@ingenieria.usac.edu.gt>
2021-09-08 22:13:03 -0600

    [ADD] Inicio app

---
+++ Proyecto 1/smart-chair-app/src/App.js
@@ -0,0 +1,25 @@
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Dashboard from './page/dashboard/Dashboard'
+import Control  from './page/control/Control'
+import Configure from './page/configure/Configure'
+import Report from './page/report/Report'
+import Navbar from './components/Navbar/Navbar';
+
+import 'bootswatch/dist/flatly/bootstrap.min.css'
+
+function App() {
+  return (
+    <Router>
+      <Navbar/>
+      <Switch>
+        <Route exact path="/" component={Dashboard} />
+        <Route exact path="/configure" component={Configure} />
+        <Route exact path="/control" component={Control} />
+        <Route exact path="/report" component={Report} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
