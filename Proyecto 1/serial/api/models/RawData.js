c8ed25fcf72d04066398adc78971b28194f18af1
PabloC54 <3091865790609@ingenieria.usac.edu.gt>
2021-09-16 23:46:24 -0600

    minor fixes

--- Proyecto 1/serial/api/models/RawData.js
+++ Proyecto 1/serial/api/models/RawData.js
@@ -2,14 +2,11 @@ import mongoose from 'mongoose'
 
 const { Schema, model } = mongoose
 
-const RawDataSchema = new Schema(
-  {
-    peso: Number,
-    proximidad: Number,
-    fecha: Date
-  },
-  { _id: false }
-)
+const RawDataSchema = new Schema({
+  peso: Number,
+  proximidad: Number,
+  fecha: Date
+})
 
 const RawData = model('rawData', RawDataSchema)
 
