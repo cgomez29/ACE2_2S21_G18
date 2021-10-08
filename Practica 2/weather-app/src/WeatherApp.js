de466b1213959ad5af21c0d9fa44b1c42bd281d6
Elmer Gustavo Sanchez Garcia <54461105+gstavosanchez@users.noreply.github.com>
2021-10-08 00:55:25 -0600

    ADD Sidebar

---
+++ Practica 2/weather-app/src/WeatherApp.js
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { Dashboard } from './components/dashboad/Dashboard';
+import { Navbar } from './components/nav/Navbar';
+
+export const WeatherApp = () => {
+    return (
+        <BrowserRouter>
+            <div className="main-content">
+                <Navbar />
+                <Switch>
+                    <Route path="/" exact component={Dashboard} />
+                    <Redirect to="/" />
+                </Switch>
+            </div>
+            <Dashboard />
+        </BrowserRouter>
+    );
+};
