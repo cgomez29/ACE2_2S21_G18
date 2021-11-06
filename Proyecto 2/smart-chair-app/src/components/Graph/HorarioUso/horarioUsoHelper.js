d7cba0c8082599fc0b620f2ed7325d551be94061
Elmer Gustavo Sanchez Garcia <54461105+gstavosanchez@users.noreply.github.com>
2021-11-05 18:03:11 -0600

    Update horario

--- Proyecto 2/smart-chair-app/src/components/Graph/HorarioUso/horarioUsoHelper.js
+++ Proyecto 2/smart-chair-app/src/components/Graph/HorarioUso/horarioUsoHelper.js
@@ -4,58 +4,69 @@ import { getAVG } from '../../../helpers/servicesAPI';
 export const getHorarioUso = async () => {
     const dayList = [];
     const usoList = [];
-    let total = 0;
     const res = await getAVG();
-    const { data } = res.data;
-    data.map(({ dia, uso }) => {
-        dayList.push(getDateByDay(dia));
+    const { tiempo_promedio, semana } = res.data;
+    console.log(tiempo_promedio);
+    console.log(semana);
+    semana.map(({ dia, uso, fecha }) => {
+        dayList.push(getDateByDay(fecha));
         usoList.push(uso);
-        total += uso;
+        // total += uso;
     });
     return {
         diaLista: dayList,
         usoLista: usoList,
-        total: total,
+        total: tiempo_promedio,
     };
 };
 
-const getDateByDay = (day) => {
-    // FIXME: solo para sabados funciona
-    const dateNow = moment().format('dddd Do MMM YY');
-    switch (day) {
-        case 'domingo':
-            return moment()
-                .subtract(6, 'days')
-                .format('dddd Do MMM YY')
-                .toString();
-        case 'lunes':
-            return moment()
-                .subtract(5, 'days')
-                .format('dddd Do MMM YY')
-                .toString();
-        case 'martes':
-            return moment()
-                .subtract(4, 'days')
-                .format('dddd Do MMM YY')
-                .toString();
-        case 'miercoles':
-            return moment()
-                .subtract(3, 'days')
-                .format('dddd Do MMM YY')
-                .toString();
-        case 'jueves':
-            return moment()
-                .subtract(2, 'days')
-                .format('dddd Do MMM YY')
-                .toString();
-        case 'viernes':
-            return moment()
-                .subtract(1, 'days')
-                .format('dddd Do MMM YY')
-                .toString();
-        case 'sabado':
-            return dateNow.toString();
-        default:
-            return dateNow.toString();
-    }
+// const getDateByDay = (day) => {
+//     // FIXME: solo para sabados funciona
+//     const dateNow = moment().format('dddd Do MMM YY');
+//     switch (day) {
+//         case 'domingo':
+//             return moment()
+//                 .subtract(6, 'days')
+//                 .format('dddd Do MMM YY')
+//                 .toString();
+//         case 'lunes':
+//             return moment()
+//                 .subtract(5, 'days')
+//                 .format('dddd Do MMM YY')
+//                 .toString();
+//         case 'martes':
+//             return moment()
+//                 .subtract(4, 'days')
+//                 .format('dddd Do MMM YY')
+//                 .toString();
+//         case 'miercoles':
+//             return moment()
+//                 .subtract(3, 'days')
+//                 .format('dddd Do MMM YY')
+//                 .toString();
+//         case 'jueves':
+//             return moment()
+//                 .subtract(2, 'days')
+//                 .format('dddd Do MMM YY')
+//                 .toString();
+//         case 'viernes':
+//             return moment()
+//                 .subtract(1, 'days')
+//                 .format('dddd Do MMM YY')
+//                 .toString();
+//         case 'sabado':
+//             return dateNow.toString();
+//         default:
+//             return dateNow.toString();
+//     }
+// };
+const getDateByDay = (date) => {
+    console.log(date);
+    const format = 'dddd Do MMM YY';
+    const newDate =
+        date !== undefined
+            ? moment(date).format(format)
+            : moment().format(format);
+
+    return newDate.toString();
 };
